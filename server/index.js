c640c022c0b064db57764e575b816d5be66b63fa
Nikolay Chikovani <nchikovany1@gmail.com>
2021-03-25 00:37:17 +0300

    init

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../build')));
+app.use(bodyParser.json({limit: '10mb', extended: true}));
+
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Сервер ожидает подключения...");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
