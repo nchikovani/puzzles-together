06afdcd16d6a6f2211d05f6c611b0f5b555f2f67
Nikolay Chikovani <nchikovany1@gmail.com>
2021-03-31 19:23:09 +0300

    project refactoring

--- server/index.ts
+++ server/index.ts
@@ -8,37 +8,40 @@ const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.json({limit: '10mb', extended: true}));
 
 const server = http.createServer(app);
 const io = socketIo(server);
-
+// @ts-ignore
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-const rooms = {1: {
+
+const rooms = {1: {// @ts-ignore
   puzzle: null,
   }};
-
+// @ts-ignore
 io.on('connection', (socket) => {
   console.log('User connected');
 
   socket.on("room:create", () => {
     const roomId = uuidv4();
     if (!rooms.hasOwnProperty(roomId)) {
+      // @ts-ignore
       rooms[roomId] = {
         puzzle: null
       };
     }
     socket.emit("room", roomId);
   });
-
+  // @ts-ignore
   socket.on("room:join", (roomId) => {
     if (rooms.hasOwnProperty(roomId)) {
       socket.roomId = roomId;
       socket.join(roomId);
+      // @ts-ignore
       const puzzle = rooms[roomId].puzzle;
       puzzle && socket.emit("puzzle", puzzle.getGameData());
     } else {
