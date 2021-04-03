910bc79bbda0f595022c58bd1775a7a110b65a0e
Nikolay Chikovani <nchikovany1@gmail.com>
2021-04-04 00:19:15 +0300

    change client socketService

--- client/src/service/socketService.ts
+++ client/src/service/socketService.ts
@@ -1,9 +1,20 @@
 import {io} from "socket.io-client";
 import {OptionTypes, UpdateTypes} from '../../../shared/Game.types'
 import store from '../store';
-import {setGameData, setUpdate, setRoomId, setNotFound, setOptions, setIsSolved} from '../store/actions';
+import {setGameData, setUpdate, setOptions, setIsSolved} from '../store/actions';
+import * as actions from "../../../shared/webSocketActions";
+import {
+  createAction,
+  getOptionsAction,
+  joinAction,
+  setUpdateAction
+} from "../../../shared/webSocketActions";
+import * as actionTypes from "../../../shared/webSocketActionsTypes";
 
 const SERVER_URL = 'http://localhost:8080';
+
+type InferValueTypes<T> = T extends { [key: string]: infer U} ? U : never;
+type ActionTypes = ReturnType<InferValueTypes<typeof actions>>;
 
 export default class socketService {
   socket;
@@ -12,30 +23,9 @@ export default class socketService {
       transports: ['websocket', 'polling', 'flashsocket'],
       autoConnect: false,
     });
+    this.puzzleHandlers = this.puzzleHandlers.bind(this);
 
-    //проверять входящие данные?
-    this.socket.on('puzzle:options', (options) => {
-      store.dispatch(setOptions(options));
-    });
-    this.socket.on('puzzle', (gameData) => {
-      console.log('puzzle gameData');
-      store.dispatch(setGameData(gameData));
-    });
-    this.socket.on('puzzle:getUpdate', (update) => {
-      console.log('update from server');
-      store.dispatch(setUpdate(update));
-    });
-    this.socket.on('puzzle:solved', () => {
-      console.log('solved');
-      store.dispatch(setIsSolved(true));
-    });
-    this.socket.on('room', (roomId) => {
-      store.dispatch(setRoomId(roomId));
-    });
-    this.socket.on('room:notFound', () => {
-      store.dispatch(setNotFound(true));
-    });
-
+    this.socket.on("puzzle", this.puzzleHandlers);
   }
   connect() {
     this.socket.connect();
@@ -45,19 +35,39 @@ export default class socketService {
     this.socket.disconnect();
   }
 
-  getOptions(image: string) {
-    this.socket.emit('puzzle:getOptions', image);
+  puzzleHandlers(action: ActionTypes) {
+    switch (action.type) {
+      case actionTypes.OPTIONS:
+        store.dispatch(setOptions(action.options));
+        break;
+      case actionTypes.GAME_DATA:
+        store.dispatch(setGameData(action.gameData));
+        break;
+      case actionTypes.UPDATE:
+        store.dispatch(setUpdate(action.update));
+        break;
+      case actionTypes.SOLVED:
+        store.dispatch(setIsSolved(true));
+        break;
+      case actionTypes.ERROR:
+        console.log(action.error);
+        break;
+    }
   }
 
   joinRoom(roomId: string) {
-    this.socket.emit('room:join', roomId);
+    this.socket.emit('room', joinAction(roomId));
+  }
+
+  getOptions(image: string) {
+    this.socket.emit('puzzle', getOptionsAction(image));
   }
 
   createPuzzle(option: OptionTypes) {
-    this.socket.emit('puzzle:create', option);
+    this.socket.emit('puzzle', createAction(option));
   }
 
   sendUpdate (update: UpdateTypes) {
-    this.socket.emit('puzzle:setUpdate', update);
+    this.socket.emit('puzzle', setUpdateAction(update));
   }
 }
